5d6cfdd7dabcaf62e4c9c19d2feab5bf14be19ce
Aayush B <aayushbahendwar@gmail.com>
2017-02-02 16:21:02 -0500

    first

--- BackEnd/studentsRecords/models/studentsRecordsDB.js
+++ BackEnd/studentsRecords/models/studentsRecordsDB.js
@@ -9,12 +9,12 @@ var studentsSchema = mongoose.Schema(
         DOB: Date,
         photo: String,
         resInfo: {type: mongoose.Schema.ObjectId, ref: 'Residencies'}
+        //,advInfo: [{type: mongoose.Schema.ObjectId, ref: 'AdvancedStandings'}]
+        //,resInfo: {type: mongoose.Schema.ObjectId, ref: 'Genders'}
+        //,advInfo: [{type: mongoose.Schema.ObjectId, ref: 'ScholarshipAwards'}]
     }
 );
 studentsSchema.plugin(mongoosePaginate);
-
-
-//TEST TEST TEST
 
 var residencySchema = mongoose.Schema(
     {
@@ -22,6 +22,39 @@ var residencySchema = mongoose.Schema(
         students: [{type: mongoose.Schema.ObjectId, ref: ('Students')}]
     }
 );
+
+//////////////////////////////////////////////// Phase 1
+
+var advancedStandingSchema = mongoose.Schema(
+    {
+        course: String,
+        description: String,
+        units: String,
+        grade: String,
+        from: String, // the school that this course is from
+        studentInfo:  {type: mongoose.Schema.ObjectId, ref: 'Students'}
+    }
+);
+
+var genderSchema = mongoose.Schema(
+    {
+        name: String,
+        students: [{type: mongoose.Schema.ObjectId, ref: ('Students')}]
+    }
+);
+
+var scholarshipAwardSchema = mongoose.Schema(
+    {
+        note: String,
+        studentInfo:  {type: mongoose.Schema.ObjectId, ref: 'Students'}
+    }
+);
+
+var ScholarshipAwards = mongoose.model('scholarshopAward', scholarshipAwardSchema);
+
+var AdvancedStandings = mongoose.model('advancedStanding', advancedStandingSchema);
+var Genders = mongoose.model('gender', genderSchema);
+////////////////////////////////////////////////
 
 var Students = mongoose.model('student', studentsSchema);
 var Residencies = mongoose.model('residency', residencySchema);
