c171c69beaef376d0d90bc6da8d9c764d9c5fd07
Aayush B <aayushbahendwar@gmail.com>
2017-02-04 14:11:32 -0500

    added delete to residency

--- BackEnd/studentsRecords/routes/residencies.js
+++ BackEnd/studentsRecords/routes/residencies.js
@@ -54,6 +54,14 @@ router.route('/:residency_id')
                 });
             }
         })
+    .delete(parseUrlencoded, parseJSON, function (request, response) {
+        models.Residencies.findByIdAndRemove(request.params.residency_id,
+            function (error, deleted) {
+                if (!error) {
+                    response.json({residency: deleted});
+                }
+            }
+        );
     });
 
 module.exports = router;
