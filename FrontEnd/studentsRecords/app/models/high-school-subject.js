1ca9136b2d4f1e21735ddb85e4c40080ed06bb07
Kripal Patel <kpate222@uwo.ca>
2017-02-22 22:39:45 -0500

    delete

--- FrontEnd/studentsRecords/app/models/high-school-subject.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-name: DS.attr(),
-description: DS.attr(),
-highSchoolCourses: DS.hasMany('high-school-courses'),
-});
--- FrontEnd/studentsRecords/app/models/term-code.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-name: DS.attr(),
-programRecordInfo: DS.hasMany('program-record'),
-});
