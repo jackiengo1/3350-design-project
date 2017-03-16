23bd065d32736a2dc81e4b92627c326cfccc62f7
Jackie Ngo <jngo42@uwo.ca>
2017-03-16 10:47:22 -0400

    push small changes

--- FrontEnd/studentsRecords/app/components/file-upload.js
+++ FrontEnd/studentsRecords/app/components/file-upload.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  /* golbal XLSX */
+  /* global XLSX */
   residencyModel:null,
   advancedStandingModel:null,
   scholarshipAwardModel:null,
--- FrontEnd/studentsRecords/app/components/student-assessment.js
+++ FrontEnd/studentsRecords/app/components/student-assessment.js
@@ -12,20 +12,28 @@ export default Ember.Component.extend({
   actions: {
 
     generatePDFs(){
-      var docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+      var docDefinition = {
+        info: {
+          title: 'awesome Document',
+          author: 'john doe',
+          subject: 'subject of document',
+          keywords: 'keywords for document',
+        },
+        content: 'This is an sample PDF printed with pdfMake'
+      };
       pdfMake.createPdf(docDefinition).open();
     },
 
     generateExcelFiles(){
 
       var data = [
-      ['Title 1', 'Title 2', 'Title 3'],
-      ['row1cell1', 'row1cell2', 'row1cell3'],
-      ['row2cell1', 'row2cell2', 'row2cell3']
-  ];
+        ['Title 1', 'Title 2', 'Title 3'],
+        ['row1cell1', 'row1cell2', 'row1cell3'],
+        ['row2cell1', 'row2cell2', 'row2cell3']
+      ];
 
 
-    this.get('excel').export(data, 'sheet1', 'test.xlsx');
+      this.get('excel').export(data, 'sheet1', 'test.xlsx');
 
 
     },
