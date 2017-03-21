95afbc8dea32d4fb4f4827f7673723e98be433c5
Kai	Wang <kwang268@uwo.ca>
2017-03-20 21:27:05 -0400

    prepull push

--- FrontEnd/studentsRecords/app/components/logical-expression.js
+++ FrontEnd/studentsRecords/app/components/logical-expression.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
   courseModel:null,
-  boolExpression:Ember.A(["=",">","<",">=","<=","Required"]),
+  boolExpression:Ember.A(["=",">","<","Required"]),
   logicalLink:Ember.A(["and","or"]),
   assessmentCodeModel:null,
   logicalExpModel:null,
@@ -14,9 +14,10 @@ export default Ember.Component.extend({
   inputValue:null,
   numberFieldVisable:false,
   selectedAessmentCode:null,
-  selectedLogicalExp:null,
   selectedlogicalLink:null,
   showLogic:false,
+  logicalExpArray:Ember.A(),
+  logicalLinkArray:Ember.A(),
 
   init() {
     this._super(...arguments);
@@ -45,7 +46,7 @@ export default Ember.Component.extend({
     selectBool(bool){
       this.set('selectedBool',bool);
 
-      if(bool === "=" || bool ===">" || bool ==="<" || bool ===">=" || bool ==="<=")
+      if(bool === "=" || bool ===">" || bool ==="<")
       {
         //set the number field to show, so that user can type the value there
         this.set('numberFieldVisable',true);
@@ -60,17 +61,8 @@ export default Ember.Component.extend({
       this.set('selectedAessmentCode',assessment);
     },
 
-    selectLogicalExp(logicalExp){
-      this.set('selectedLogicalExp',logicalExp);
-    },
-
     selectlogicLink(link){
       this.set('selectedlogicalLink',link);
-    },
-
-    toggleShowLogic(){
-      //each time this function is called, toggle the boolean value in the show logic
-      this.toggleProperty('showLogic');
     },
 
     addExpression(){
@@ -79,22 +71,26 @@ export default Ember.Component.extend({
       if(this.get('numberFieldVisable'))
       {
         numberFieldValue = this.get('inputValue');
-        console.log(numberFieldValue);
       }
-      var booleanValue = this.get('selectedBool');
-      var courseValue = this.get('selectedCourse');
-      var assessmentValue = this.get('selectedAessmentCode');
-      var logicalExpValue = this.get('selectedLogicalExp');
-      var logicalLinkValue = this.get('selectedlogicalLink');
-      //combine the course boolean value into a single string
-      var booleanExpString = courseValue+booleanValue+numberFieldValue;
-      //create a new logical expression record using the combined string
-      this.get('store').createRecord('logical-expression',{
-        booleanExp: booleanExpString,
-        logicalLink:logicalLinkValue,
-        comment: assessmentValue,
-        link: logicalExpValue,
-      });
+      //after adding the first expression, show the logical link area
+      this.get('showLogic',true);
+       var booleanValue = this.get('selectedBool');
+       var courseValue = this.get('selectedCourse');
+       //combine the course boolean value into a single string
+       var booleanExpString = courseValue+" "+booleanValue+" "+numberFieldValue;
+       //add the expession string into logical expression array
+       this.get('logicalExpArray').pushObject(booleanExpString);
+       //if this is not the first run, add the logic link to the logical link array
+       if(this.get('showLogic'))
+       {
+         let logiclinktemp = this.get('selectedlogicalLink');
+         this.get('logicalLinkArray').pushObject(logiclinktemp);
+       }
+       //after adding the existing data into the array, clean all data
+       this.set('inputValue',null);
+       this.set('selectedBool',null);
+       this.set('selectedCourse',null);
+       this.set('selectedlogicalLink',null);
     },
   }
 });
--- FrontEnd/studentsRecords/app/models/logical-expression.js
+++ FrontEnd/studentsRecords/app/models/logical-expression.js
@@ -4,5 +4,4 @@ export default DS.Model.extend({
   booleanExp: DS.attr(),
   logicalLink: DS.attr(),
   comment: DS.belongsTo('assessment-code'),
-  link: DS.hasMany('logical-expression'),
 });
