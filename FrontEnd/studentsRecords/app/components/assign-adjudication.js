d60241af5ce49be03b68f2bd09631aa383632554
Kai	Wang <kwang268@uwo.ca>
2017-03-22 11:08:36 -0400

    adjudication fix needed

--- FrontEnd/studentsRecords/app/components/assign-adjudication.js
+++ FrontEnd/studentsRecords/app/components/assign-adjudication.js
@@ -57,7 +57,7 @@ export default Ember.Component.extend({
     });
   },
 
-  action:{
+  actions:{
     //for each student, put all course (throught different terms) into a single array for that student
     orgainzeStudentCourses(){
       var allStudents = this.get('studentModel');
@@ -69,9 +69,15 @@ export default Ember.Component.extend({
         //search through the term with the student number
         for(let j=0;j<allterms.get('length');j++)
         {
+          console.log(allterms.objectAt(j).get('studentInfo').get('firstName'));
+//!<<<<<<<<<<<this part need debug, allterms.objectAt(j).get('studentInfo') is always null
+//in addition, if i do this.get('student').get('semester') the returned array has zero length
+
+
           //if the student object equal to the student reference in term
-            if(allStudents.objectAt(i)== allterms.objectAt(j).get('studentInfo'))
+            if(allStudents.objectAt(i).get('id')== allterms.objectAt(j).get('studentInfo').get('id'))
             {
+              console.log(1);
               //get all courses in that term
               var termCourses = allterms.objectAt(j).get('courseInfo');
               //for each termcourses, add it to the student course array
@@ -79,12 +85,17 @@ export default Ember.Component.extend({
               {
                 //always append the termcourse obj to the end of student course array
                 studentcourseArray[studentcourseArray.get('length')] = termCourses.objectAt(k);
+                console.log(termCourses.objectAt(k));
               }
             }
         }
         //after checking through all terms for the current student, add the student course array (including all courses cross terms) to the ember course array
         this.get('studentTermArray').pushObject(studentcourseArray);
       }
+      //for(let i=0;i<this.get('studentTermArray').get('length');i++)
+      //{console.log(this.get('studentTermArray').objectAt(i));}
+      //after this function call, call the adjudicationStudent function
+      //this.send('adjudicationStudent');
     },
 
     adjudicationStudent()
@@ -96,20 +107,35 @@ export default Ember.Component.extend({
         let currentAssCode = allAssessmentCode.objectAt(i);
         let currentlogicExp = currentAssCode.get('testExpression');
         let logicalExpArray = currentlogicExp.get('booleanExp');
+        //!>>>>>>Pay attention to the logical link array, the first element [0] is "", starting from the second one is the actual and/or
         let logicalLinkArray = currentlogicExp.get('logicalLink');
         //logicalExpArray is sth like a>50, b Required...etc has to be splited
-        var expressionCourseArray = [];
-        var expressionBooleanArray =[];
+        var expressionCourseArray = [];//this array hold the course name in the logical exp
+        var expressionBooleanArray =[];//this array hold the
         var expressionNumberArray = [];
+        var requiredCourseArray= [];
         //for each logical exp inside the logcal exp array
         for(let j=0;j<logicalExpArray.get('length');j++)
         {
           let currentExp = logicalExpArray.objectAt(j); //currentexp is a string including a>b ...etc
           let splitedExp = currentExp.split("-"); //split with "-" <-this is manually added in the logical expression class for easy separation
-          
+          //check if the course is a required course  [0] should be the course name [1] is the operator [2]if exist is the value
+          if(splitedExp[1] === "Required")
+          {
+            //if the course is required only add it to requiredCourseArray so the exp arrays always have correct index corresponding
+            requiredCourseArray[requiredCourseArray.length] = splitedExp[0];
+          }
+          else{
+            //if the course is not required, it has some value requirement, add the value into the corresponding arrays
+            expressionCourseArray[expressionCourseArray.length] = splitedExp[0];
+            expressionBooleanArray[expressionBooleanArray.length] = splitedExp[1];
+            expressionNumberArray[expressionNumberArray.length] = splitedExp[2];
+          }
         }
       }
     },
+
+
 
   }
 
