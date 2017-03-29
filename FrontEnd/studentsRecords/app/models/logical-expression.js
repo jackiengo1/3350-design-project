36ef0116cb463c8717dcf8e0a2ccaf9eac2ce851
Jackie Ngo <jngo42@uwo.ca>
2017-03-29 11:57:56 -0400

    added component

--- FrontEnd/studentsRecords/app/models/logical-expression.js
+++ FrontEnd/studentsRecords/app/models/logical-expression.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   booleanExp: DS.attr(),
   logicalLink: DS.attr(),
+  link: DS.hasMany('logical-expression'),
   comment: DS.belongsTo('assessment-code'),
 });
