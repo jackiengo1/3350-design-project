f42b865bd1a0c733eeb46fedb48628a96d32de28
Kai	Wang <kwang268@uwo.ca>
2017-03-31 21:33:31 -0400

    logical expression v2 done

--- FrontEnd/studentsRecords/app/components/logical-expression-2.js
+++ FrontEnd/studentsRecords/app/components/logical-expression-2.js
@@ -23,6 +23,15 @@ export default Ember.Component.extend({
   //switches for visiablility of the text field for mark
   numberFieldVisable:false,
 
+  //data used for editing logical expression
+  loigcalExpForEdit:null,
+  courseForEdit:null,
+  operatorForEdit:null,
+  markForEdit:null,
+  linkForEdit:null,
+  assessmentForEdit:null,
+  ExpArrayForEdit:null,
+
   init() {
     this._super(...arguments);
     var self = this;
@@ -66,7 +75,13 @@ export default Ember.Component.extend({
     this.set('assessmentCodeModel',this.get('store').findAll('assessment-code'));
   },
 
+  didRender() {
+    Ember.$('.menu .item').tab();
+  },
+
+
   actions: {
+
     selectCourse(course){
       this.set('selectedCourse',course);
     },
@@ -94,12 +109,6 @@ export default Ember.Component.extend({
     },
 
     selectLogicalExp(exp){
-      //if the logical expressin array is not null, then a logical link must be selected
-      // if(this.get('showLogic')&& this.get('selectedlogicalLink')=="" && selectedLogicalExpArray.get('length') === 0)
-      // {
-      //   alert("Logical Link can not be empty!");
-      //   //this.set('errorMsg',"Logical Link can not be empty!");
-      // }
       this.set('selectedLogicalExp',exp);
     },
 
@@ -115,6 +124,8 @@ export default Ember.Component.extend({
         this.get('selectedLogicalExpArray').pushObject(selectedexp);
         //delete this logical expression from the currentLogicExpList
         this.get('currentLogicExpList').removeObject(selectedexp);
+        //clear out the selectedLogicalExp after adding
+        this.set('selectedLogicalExp',null);
       }
       else{
         alert("You must select a logical expression before adding");
@@ -123,6 +134,7 @@ export default Ember.Component.extend({
 
     //construct and post the new logical expresion object to the backend
     postLogicalExp(){
+      var self = this;
       //if number field is visiable assign the value otherwise keep it null
       var numberFieldValue = "";
       if(this.get('numberFieldVisable'))
@@ -160,23 +172,157 @@ export default Ember.Component.extend({
           link: logicalExpRefArray,
           comment: tempCode,
         });
-        newExpression.save();
+        newExpression.save().then(function(record){
+          let allLogic = self.get('store').peekAll('logical-expression');
+          //clear the currentLogicExpList array
+          self.get('currentLogicExpList').clear();
+          //after saving the new logical expression refreash the currentLogicExpList array
+          for(let i=0;i<allLogic.get('length');i++)
+          {
+            self.get('currentLogicExpList').pushObject(allLogic.objectAt(i));
+          }
+        });
       }
     },
 
     deleteSelectedExp(selectedExp){
-        //delete the selectedExp from the selectedLogicalExpArray
-        this.get('selectedLogicalExpArray').removeObject(selectedExp);
-        //after deleted from the selected array add it back to the option array
-        this.get('currentLogicExpList').pushObject(selectedExp);
+      //delete the selectedExp from the selectedLogicalExpArray
+      this.get('selectedLogicalExpArray').removeObject(selectedExp);
+      //after deleted from the selected array add it back to the option array
+      this.get('currentLogicExpList').pushObject(selectedExp);
+    },
+
+    deleteRemoteExp(exp){
+      //delete the logiical expression from remote
+      let tempExp = exp;
+      tempExp.deleteRecord();
+      tempExp.save();
     },
 
     openEditLogicalExpForm(selectedExp){
-      // this.set('planCodeToEdit', planCode);
-      // this.set('planCodeNameEdit', planCode.get('name'));
-      // console.log(this.get('planCodeToEdit'));
-      // Ember.$('.ui.modal.planCodeEdit').modal({detachable: false,}).modal('show');
+      //clear all selected values when start editing
+      this.set('selectedCourse',null);
+      this.set('selectedBool',null);
+      this.set('selectedAessmentCode',null);
+      this.set('selectedLogicalExp',null);
+      this.set('selectedlogicalLink',"");
+      this.set('inputValue',null);
+      this.set('numberFieldVisable',false);
+
+      //set the logcial expression for editing
+      this.set('loigcalExpForEdit', selectedExp);
+      let splitedExp = selectedExp.get('booleanExp').split("  ");
+      this.set('courseForEdit',splitedExp[0]);
+      this.set('operatorForEdit',splitedExp[1]);
+      if(splitedExp[1] !=="Required")
+      {
+        //if the operator is not required then mark exist, else mark does not exsit.
+        this.set('markForEdit',splitedExp[2]);
+        //display the mark field
+        this.set('numberFieldVisable',true);
+      }
+      //set the link for edit
+      this.set('linkForEdit',selectedExp.get('logicalLink'));
+      //set the assessment code for edit
+      this.set('assessmentForEdit',selectedExp.get('comment'));
+      //set the logical expression array for edit
+      this.set('ExpArrayForEdit',selectedExp.get('link'));
+
+      //display the edit model
+      Ember.$('.ui.modal.logicalExpEdit').modal({detachable: false,}).modal('show');
+    },
+
+
+    //construct and post the new logical expresion object to the backend
+    saveLogicalExpChanges(){
+      var self = this;
+      //if number field is visiable assign the value otherwise keep it null
+      var numberFieldValue = "";
+      if(this.get('numberFieldVisable'))
+      {
+        numberFieldValue = this.get('markForEdit');
+      }
+      var booleanValue = this.get('selectedBool');
+      if(booleanValue == "null" || booleanValue == null)
+      {
+        alert("You must select an operator!");
+        //this.set('errorMsg',"You must select an operator!" );
+      }
+      else if (this.get('numberFieldVisable') && this.get('markForEdit')==null && this.get('markForEdit')=="")
+      {
+        alert("Number field can not be empty!");
+        //this.set('errorMsg',"Number field can not be empty!");
+      }
+      else if (this.get('selectedCourse')==null || this.get('selectedCourse') =="")
+      {
+        alert("Course can not be empty!");
+        //this.set('errorMsg',"Logical Link can not be empty!");
+      }
+      else{
+        //if all mandantory infomation is filled, then proceed
+        //compress the couse name + boolean operator + mark if mark exist
+        let combinedExp = this.get('selectedCourse')+"  "+this.get('selectedBool')+"  "+numberFieldValue;
+        let logicalLink = this.get('selectedlogicalLink');
+        let logicalExpRefArray = this.get('ExpArrayForEdit');
+        //if selectedAessmentCode is not null find the object in local cache, if null tempcode will be null as well since nothing found
+        let tempCode = this.get('store').peekRecord('assessment-code',this.get('selectedAessmentCode'));
+
+        let updatedExp = this.get('loigcalExpForEdit');
+        updatedExp.set('booleanExp',combinedExp);
+        updatedExp.set('logicalLink',logicalLink);
+        if(logicalExpRefArray!=null)
+        {
+          //if the link array is not null, update the link array
+          updatedExp.set('link',logicalExpRefArray);
+        }
+        updatedExp.set('comment',tempCode);
+        updatedExp.save().then(function(record){
+
+          //clear all selected values
+          self.set('selectedCourse',null);
+          self.set('selectedBool',null);
+          self.set('selectedAessmentCode',null);
+          self.set('selectedLogicalExp',null);
+          self.set('selectedlogicalLink',"");
+          self.set('inputValue',null);
+          self.set('numberFieldVisable',false);
+
+          self.set('loigcalExpForEdit',null);
+          self.set('courseForEdit',null);
+          self.set('operatorForEdit',null);
+          self.set('markForEdit',null);
+          self.set('linkForEdit',null);
+          self.set('assessmentForEdit',null);
+          self.set('ExpArrayForEdit',null);
+          Ember.$('.ui.modal.logicalExpEdit').modal('hide');
+        });
+      }
+    },
+
+    deleteSelectedEditExp(selectedExp){
+      //delete the selectedExp from the ExpArrayForEdit
+      this.get('ExpArrayForEdit').removeObject(selectedExp);
+    },
+
+    closeLogicalExpEditModal(){
+      //clear all selected values
+      this.set('selectedCourse',null);
+      this.set('selectedBool',null);
+      this.set('selectedAessmentCode',null);
+      this.set('selectedLogicalExp',null);
+      this.set('selectedlogicalLink',"");
+      this.set('inputValue',null);
+      this.set('numberFieldVisable',false);
+
+      this.set('loigcalExpForEdit',null);
+      this.set('courseForEdit',null);
+      this.set('operatorForEdit',null);
+      this.set('markForEdit',null);
+      this.set('linkForEdit',null);
+      this.set('assessmentForEdit',null);
+      this.set('ExpArrayForEdit',null);
+
+      Ember.$('.ui.modal.logicalExpEdit').modal('hide');
     },
   },
-
 });
